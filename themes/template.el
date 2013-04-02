acb76e70d5429c8354fdeed886978e69eb2803be
owain lewis <owain@owainlewis.com>
2013-04-02 16:19:23 +0100

    updates template

--- themes/template.el
+++ themes/template.el
@@ -1,51 +1,71 @@
+;; -------------------------------------
+;;
+;; Custom Theme
+;;
+;; -------------------------------------
+;; Color Guide
+;; -------------------------------------
+;;
+;; background: %{background}
+;; foreground: %{foreground}
+;; border:     %{border}
+;; cursor:     %{cursor}
+;; mouse:      %{mouse}
+;; fringe:     %{fringe}
+;; mode:       %{mode}
+;; region:     %{region}
+;; builtin:    %{builtin}
+;; comment:    %{comment}
+;; function:   %{function}
+;; keyword:    %{keyword}
+;; string:     %{string}
+;; class:      %{class}
+;; constants:  %{constant}
+;; variable:   %{variable}
+;; prompt:     %{prompt}
+;;
+;; -------------------------------------
+
 (defun your-config-name-here ()
   (interactive)
-
   (color-theme-install
    '(your-config-name-here
-      ((background-color . "#101e2e")
-      (background-mode . light)
-      (border-color . "#ed5050")
-      (cursor-color . "#978c35")
-      (foreground-color . "#eeeeec")
-      (mouse-color . "black"))
-
+      ((background-color . "%{background}")
+      (background-mode . dark)
+      (border-color . "%{border}")
+      (cursor-color . "%{cursor}")
+      (foreground-color . "%{foreground}")
+      (mouse-color . "%{mouse}"))
      ;; Frame borders
-     (fringe ((t (:background "#ed5050"))))
-
+     (fringe ((t (:background "%{border}"))))
      ;; The bottom active window color
-     (mode-line ((t (:foreground "#dbdb9a" :background "#555753"))))
-
+     (mode-line ((t (:foreground "%{foreground}" :background "%{mode}"))))
      ;; Selection
-     (region ((t (:background "#7a907e"))))
-
+     (region ((t (:background "%{region}"))))
      ;; Built in language constructs
-     (font-lock-builtin-face ((t (:foreground "#729fcf"))))
-
+     (font-lock-builtin-face ((t (:foreground "%{builtin}"))))
      ;; Comments
-     (font-lock-comment-face ((t (:foreground "#888a85"))))
-
+     (font-lock-comment-face ((t (:foreground "%{comment}"))))
      ;; Functions
-     (font-lock-function-name-face ((t (:foreground "#edd400"))))
-
+     (font-lock-function-name-face ((t (:foreground "%{function}"))))
      ;; Keywords
-     (font-lock-keyword-face ((t (:foreground "#729fcf"))))
-
+     (font-lock-keyword-face ((t (:foreground "%{keyword}"))))
      ;; Strings
-     (font-lock-string-face ((t (:foreground "#ad7fa8"))))
-
+     (font-lock-string-face ((t (:foreground "%{string}"))))
      ;; Classes
-     (font-lock-type-face ((t (:foreground"#8ae234"))))
-
+     (font-lock-type-face ((t (:foreground "%{class}"))))
      ;; Constants
-     (font-lock-constant-face ((t (:foreground "#eeeeec"))))
-
+     (font-lock-constant-face ((t (:foreground "%{constant}"))))
      ;; Variables
-     (font-lock-variable-name-face ((t (:foreground "#eeeeec"))))
-
+     (font-lock-variable-name-face ((t (:foreground "%{variable}"))))
      ;; Bottom buffer prompt (i.e M-x color)
-     (minibuffer-prompt ((t (:foreground "#4b545d" :bold t))))
+     (minibuffer-prompt ((t (:foreground "%{prompt}" :bold t))))
 
      (font-lock-warning-face ((t (:foreground "red" :bold t)))))))
 
+;; -------------------------------------
+
 (provide 'your-config-name-here)
+
+;; -------------------------------------
+

387761bd2a5295cc42412231751fba0934687034
Will Speak <lithiumflame@gmail.com>
2013-06-29 10:33:14 +0100

    Update Install Script
    Add Fogus to the installation script

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ mkdir -p ~/.emacs.d/themes
 echo "Installing hickey"
 cp themes/hickey-theme.el ~/.emacs.d/themes/hickey-theme.el
 
+echo "Installing Fogus"
+cp themes/fogus-theme.el ~/.emacs.d/themes/fogus-theme.el
+
 echo "Done..\n"
 echo "Remember to include the themes in your .emacs config i.e (load-theme 'hickey t)\n\n"

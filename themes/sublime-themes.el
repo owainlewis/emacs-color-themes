7a36062d6ba958b2e84910865b39eaec4d832c72
Owain Lewis <owain@owainlewis.com>
2013-08-20 18:23:02 +0100

    Update sublime-themes.el

--- themes/sublime-themes.el
+++ themes/sublime-themes.el
@@ -25,10 +25,9 @@
 
 ;;;###autoload
 (when (boundp 'custom-theme-load-path)
-  (add-to-list 'custom-theme-load-path (file-name-as-directory
-                                        (file-name-directory load-file-name))))
-
-
+  (add-to-list 'custom-theme-load-path 
+    (file-name-as-directory
+      (file-name-directory load-file-name))))
 
 (provide 'sublime-themes)
 ;;; sublime-themes.el ends here

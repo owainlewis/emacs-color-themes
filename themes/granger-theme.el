9381c01906fcfe5fcb498acfb6abda1aac66be3d
Owain Lewis <owain@owainlewis.com>
2013-11-25 09:21:46 +0000

    Update granger-theme.el

--- themes/granger-theme.el
+++ themes/granger-theme.el
@@ -11,11 +11,11 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; ---------------------------
+;; *****************************************************************************************
 ;;
 ;; Granger: Color theme based on light table
 ;;
-;; ----------------------------
+;; *****************************************************************************************
 
 (unless (>= 24 emacs-major-version)
   (error "requires Emacs 24 or later."))
@@ -26,37 +26,50 @@
     'granger
     '(linum-format " %7i "))
 
+  (let ((background "#111013")
+        (selection  "#11253A"))
+
   (custom-theme-set-faces
    'granger
 
-   '(default                          ((t (:foreground "#ABAEB3" :background "#222222"))))
-   '(region                           ((t (:background "#101010"))))
-   '(cursor                           ((t (:background "#ffffff"))))
-   '(fringe                           ((t (:background "#2f2f2f" :foreground "#ffffff"))))
-   '(linum                            ((t (:background "#202020" :foreground "#2f2f2f" 
-                                           :box nil :height 120))))
-   '(minibuffer-prompt                ((t (:foreground "#9489C4" :weight bold))))
-   '(minibuffer-message               ((t (:foreground "#ffffff"))))
-   '(mode-line                        ((t (:foreground "#FFFFFF" :background "#484879"))))
-   '(mode-line-inactive               ((t (:foreground "#777777" :background "#303030" 
-                                           :weight light :box nil :inherit (mode-line)))))
+;; Default colors
+;; *****************************************************************************************
+
+   `(default                          ((t (:foreground "#ABAEB3" :background ,background))))
+   `(region                           ((t (:background ,selection                       ))))
+   `(cursor                           ((t (:background "#ffffff"                        ))))
+   `(fringe                           ((t (:background "#2f2f2f"   :foreground "#ffffff"))))
+   `(linum                            ((t (:background ,background :foreground "#2f2f2f"))))
+   `(minibuffer-prompt                ((t (:foreground "#9489C4"            :weight bold))))
+   `(minibuffer-message               ((t (:foreground "#ffffff"                        ))))
+   `(mode-line                        ((t (:foreground "#FFFFFF" :background "#484879"  ))))
+   `(mode-line-inactive               ((t (:foreground "#777777" :background "#303030"  ))))
+
+;; Font lock faces
+;; *****************************************************************************************
  
-   '(font-lock-keyword-face           ((t (:foreground "#3ca380"))))
-   '(font-lock-type-face              ((t (:foreground "#484879"))))
-   '(font-lock-constant-face          ((t (:foreground "#3F5C70"))))
-   '(font-lock-variable-name-face     ((t (:foreground "#547B96"))))
-   '(font-lock-builtin-face           ((t (:foreground "#6767AE"))))
-   '(font-lock-string-face            ((t (:foreground "#699ABC"))))
-   '(font-lock-comment-face           ((t (:foreground "#496b83"))))
-   '(font-lock-comment-delimiter-face ((t (:foreground "#666688"))))
+   `(font-lock-keyword-face           ((t (:foreground "#3ca380"))))
+   `(font-lock-type-face              ((t (:foreground "#484879"))))
+   `(font-lock-constant-face          ((t (:foreground "#3F5C70"))))
+   `(font-lock-variable-name-face     ((t (:foreground "#547B96"))))
+   `(font-lock-builtin-face           ((t (:foreground "#6767AE"))))
+   `(font-lock-string-face            ((t (:foreground "#699ABC"))))
+   `(font-lock-comment-face           ((t (:foreground "#496b83"))))
+   `(font-lock-comment-delimiter-face ((t (:foreground "#666688"))))
  
-   '(font-lock-function-name-face     ((t (:foreground "#3ca380"))))
-   '(font-lock-doc-string-face        ((t (:foreground "#496B83")))))
+   `(font-lock-function-name-face     ((t (:foreground "#3ca380"))))
+   `(font-lock-doc-string-face        ((t (:foreground "#496B83")))))
+
+;; *****************************************************************************************
+
+   )
 
 ;;;###autoload
 (when (and (boundp 'custom-theme-load-path) load-file-name)
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
+
+;; *****************************************************************************************
 
 (provide-theme 'granger)
 

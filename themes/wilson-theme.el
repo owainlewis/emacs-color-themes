d2f2096765e0af9c438c8f3adf0befe787c81aac
Owain Lewis <owain@owainlewis.com>
2013-12-03 14:23:14 +0000

    Update wilson-theme.el

--- themes/wilson-theme.el
+++ themes/wilson-theme.el
@@ -84,7 +84,7 @@
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name)))
   (when (not window-system)
-    (custom-set-faces '(default ((t (:background "nil")))))))
+    (custom-set-faces '(default ((t (:background nil)))))))
 
 (provide-theme 'wilson)
 
